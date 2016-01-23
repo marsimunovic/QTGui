43b9b3a58443b0b3f6d986496d45bcef18b993b0
marsimunovic <msimunovic@outlook.com>
2016-01-23 22:17:26 +0100

    Adding plotter header

---
+++ chap05/Plotter/Plotter.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-01-23T17:56:08
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Plotter
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        plotter.cpp
+
+HEADERS  += plotter.h
+
+FORMS    += plotter.ui
---
+++ chap05/Plotter/plotter.cpp
@@ -0,0 +1,60 @@
+#include "plotter.h"
+#include "ui_plotter.h"
+
+#include <QToolButton>
+
+Plotter::Plotter(QWidget *parent) :
+    QWidget(parent)
+{
+    setBackgroundRole(QPalette::Dark);
+    setAutoFillBackground(true);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    setFocusPolicy(Qt::StrongFocus);
+    rubberBandIsShown = false;
+
+    zoomInButton = new QToolButton(this);
+    zoomInButton->setIcon(QIcon(":/images/zoomin.png");
+    zoomInButton->adjustSize();
+    connect(zoomInButton, SIGNAL(clicked()), this, SLOT(zoomIn()));
+
+    zoomOutButton = new QToolButton(this);
+    zoomOutButton->setIcon(QIcon(":/image/zoomout.png"));
+    zoomOutButton->adjustSize();
+    connect(zoomOutButton, SIGNAL(clicked()), this, SLOT(zoomOut()));
+
+    setPlotSettings(PlotSettings());
+}
+
+void Plotter::setPlotSettings(const PlotSettings &settings)
+{
+    zoomStack.clear();
+    zoomStack.append(settings);
+    curZoom = 0;
+    zoomInButton->hide();
+    zoomOutButton->hide();
+    refreshPixmap();
+}
+
+void Plotter::zoomOut()
+{
+    if(curZoom > 0) {
+        --curZoom;
+        zoomOutButton->setEnabled(curZoom > 0);
+        zoomInButton->setEnabled(true);
+        zoomInButton->show();
+        refreshPixmap();
+    }
+}
+
+void Plotter::zoomOut()
+{
+    if(curZoom < zoomStack.count() - 1)
+    {
+        ++curZoom;
+        zoomInButton->setEnabled(curZoom < zoomStack.count() - 1);
+        zoomOutButton->setEnabled(true);
+        zoomOutButton->show();
+        refreshPixmap();
+    }
+}
+
